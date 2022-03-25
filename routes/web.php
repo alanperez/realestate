632366e53c92f231e6b9cc97a264485b972151aa
perezxalan <perezxalan@gmail.com>
2022-03-25 15:00:01 -0500

    setting up project

---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// HOME PAGE
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// All Listings
+Route::get('/{property_type}/{listing_type}/', function() {
+    return view('welcome');
+});
+
+// Single Listing
+Route::get('/listing/{slug}/{id}', function() {
+    return view('welcome');
+});
+
+// User Login
+Route::get('/login', function() {
+    return view('welcome');
+});
+
+// User Register
+Route::get('/register', function() {
+    return view('welcome');
+});
+
+
+// User Saved Listings
+Route::get('/acount/saved', function() {
+    return view('welcome');
+});
+
+// User Saved Listings
+Route::get('/acount/show-status', function() {
+    return view('welcome');
+});
