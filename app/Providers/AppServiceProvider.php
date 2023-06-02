1c3ffa07050de11a39f0d67427bf0fdb6da382dd
Alan Perez <perezxalan@gmail.com>
2023-06-01 22:36:48 -0500

    Update AppServiceProvider.php to deploy on render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        // Paginator::useBootstrapFive();
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
     }
 }
